41f97d7de6218b7b4199b8cc1dec1106f5bdaf6d
Samuel Ayvazyan <samuel.ayvazyan.fd@gmail.com>
2019-04-27 14:56:38 +0400

    Add business cards list display functionality.

--- src/app/modules/business-cards/business-cards.component.ts
+++ src/app/modules/business-cards/business-cards.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/shared/services/user/user.service';
 
 @Component({
   selector: 'app-business-cards',
   templateUrl: './business-cards.component.html',
-  styleUrls: ['./business-cards.component.sass']
+  styleUrls: ['./business-cards.component.scss']
 })
 export class BusinessCardsComponent implements OnInit {
+  users: any[];
 
-  constructor() { }
+  constructor(public srvUsers: UserService) { }
 
   ngOnInit() {
+    this.srvUsers.getUsers().subscribe( data => {
+      this.users = data;
+    })
   }
 
 }
--- src/app/modules/business-cards/business-cards.module.ts
+++ src/app/modules/business-cards/business-cards.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BusinessCardsComponent } from './business-cards.component';
 import { MaterialModule } from 'src/app/material.module';
+import { MatCardModule } from '@angular/material';
 
 const routes: Routes = [
   { path: '', component:  BusinessCardsComponent}
@@ -12,6 +13,7 @@ const routes: Routes = [
   imports: [
     RouterModule.forChild(routes),
     MaterialModule,
+    MatCardModule,
   ],
 })
 export class BusinessCardsModule { }
